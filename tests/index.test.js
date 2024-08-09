0a0e891cdc81b9f82dab15d0118b0cb59ac07201
Naga Vikas <pilladinagavikas@gmail.com>
2024-08-09 18:07:12 +0530

    one or two numbers testcases added

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,7 +1,17 @@
 const calculator = require('../index');
 
-describe('calculator',()=>{
-  checkSum("",0);
+describe('calculator', () => {
+  
+  describe('empty string testcase', () => {
+    checkSum("",0);
+  });
+  
+  describe('one or two numbers testcase', () => {
+    checkSum("1",1);
+    checkSum("5",5);
+    checkSum("4,2",6);
+    checkSum("88,36",124);
+  });
 
   function checkSum(inputString,output) {
     test(`evaluate "${inputString}" to ${output}`, () => {
