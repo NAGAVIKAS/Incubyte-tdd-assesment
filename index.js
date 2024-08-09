e465e550d3808b5e8d4aebb7438b75e24f622155
Naga Vikas <pilladinagavikas@gmail.com>
2024-08-09 22:16:18 +0530

    code changes for multiple custom delimiters

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var calculator = {
         var delimiters = [',','\n'];
 
         if(this.customDelimiterExists(inputString)){
-            delimiters.push(this.getCustomDelimiter(inputString));
+            delimiters = delimiters.concat(this.getCustomDelimiters(inputString));
             inputString = this.removeFirstLine(inputString);
         }
         return this.getDelimitedNumbers([inputString],delimiters);
@@ -21,13 +21,25 @@ var calculator = {
     customDelimiterExists: function(inputString) {
         return /^\/\//.test(inputString);
     },
-    getCustomDelimiter: function(inputString) {
-        return inputString.split('\n')[0].substring(2);
+    getCustomDelimiters: function(inputString) {
+        let delimiterString = inputString.split('\n')[0].substring(2);
+        if(/^\[.+\]/.test(delimiterString)){
+            let delimiters = delimiterString.split('][')
+                            .map((del) => del.indexOf('[') > -1 ? del.substring(1) :
+                                                                  (del.indexOf(']') > -1 ?
+                                                                    del.substring(0,1) : ''
+                                                                   )
+                                );
+            return delimiters;
+        } else {
+            return [delimiterString];
+        }
     },
     removeFirstLine: function(inputString) {
         return inputString.substring(inputString.indexOf('\n') + 1);
     },
     getDelimitedNumbers: function(numbersDelimited, delimiters) {
+        console.log("delimiters -> ",delimiters);
         if(delimiters.length === 0) {
             let numbersFiltered = numbersDelimited.filter((num) => num.length > 0 && parseInt(num) < 1001);
             return numbersFiltered;
